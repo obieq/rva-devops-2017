9b9293876f450e4d071f8e6acf17eff4f016fca2
Obie Quelland <obie@Tres-MacBook-Pro.local>
2017-04-26 16:01:09 -0400

    changed to port 8080

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 	app.MountOperandsController(service, c)
 
 	// Start service
-	if err := service.ListenAndServe(":80"); err != nil {
+	if err := service.ListenAndServe(":8080"); err != nil {
 		service.LogError("startup", "err", err)
 	}
 
